57ed1a580cc88d4eb4a07cff79befc7939164cf8
FarhanJujur <151694128+FarhanJujur@users.noreply.github.com>
2024-02-27 03:01:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
   TOKEN: "",
-  ownerID: ["1004206704994566164", ""],
+  ownerID: ["767923472164192307", ""],
   botInvite: "",
   supportServer: "",
   mongodbURL: "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
-  status: 'RTXX GG',
+  status: 'Hann',
   commandsDir: './commands',
   language: "en",
   embedColor: "00fbff",
@@ -13,7 +13,7 @@ module.exports = {
 
   sponsor: {
     status: true,
-    url: "https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A",
+    url: "https://www.instagram.com/hann19_8/",
   },
 
   voteManager: {
